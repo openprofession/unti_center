7b8542531aec14d40804db4efdd63b2040a3a86a
TS <tsependa.s@gmail.com>
2019-07-11 00:19:45 +0300

    add sport

--- center/dashviews/sports.py
+++ center/dashviews/sports.py
@@ -24,8 +24,10 @@ def dash_sports(request, date=(datetime.now() + timedelta(days=1)).date()):
             enrolls_event_df = enrolls_event_df.sort_values(by='free', ascending=False)
 
             result['enrolls_by_event'] = enrolls_event_df.to_dict('records')
-
-            # result['enrolls_by_event_sum'] = enrolls_event_sum_df.to_dict('records')
+            enrolls_event_top_df = enrolls_event_df.sort_values(by='userID', ascending=False)
+            result['enrolls_by_event_top'] = enrolls_event_top_df.to_dict('records')
+            print(enrolls_df.count())
+            result['user_count'] = enrolls_df['userID'].nunique()
 
     except OperationalError as e:
         print(e)
