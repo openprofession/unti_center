015152876c6cd367eea0eace6977d812e6684108
TS <tsependa.s@gmail.com>
2019-07-17 10:28:32 +0300

    add now

--- center/sql/events.py
+++ center/sql/events.py
@@ -118,7 +118,8 @@ SELECT
   timetable.createDT,
   activity.sizeMin,
   activity.sizeMax,
-  place.title AS place_title
+  place.title AS place_title,
+  place.capacity AS place_capacity
 FROM xle.timetable
   LEFT OUTER JOIN xle.event
     ON timetable.runID = event.runID
