26a628763ed6f5ebe9519c3a7f95b0c3d5e9adbf
TS <tsependa.s@gmail.com>
2019-07-19 13:03:39 +0300

    add timetable

--- center/dashviews/timetable.py
+++ center/dashviews/timetable.py
@@ -19,18 +19,19 @@ def dash_timetable(request):
             print(request.user.leader_id)
             cursor.execute(events.timetable_by_leader.format(request.user.leader_id))
             events_df = pd.DataFrame(dictfetchall(cursor))
-            events_df = events_df.sort_values('startDT', ascending=True)
-            events_df['startDT'] = pd.to_datetime(events_df['startDT'])
-            events_df['endDT'] = pd.to_datetime(events_df['endDT'])
-            events_df['start'] = events_df['startDT'].dt
-            events_df['start_h'] = events_df['startDT'].dt.hour + 3
-            events_df['start_m'] = events_df['startDT'].dt.minute
-            events_df['end_h'] = events_df['endDT'].dt.hour + 3
-            events_df['end_m'] = events_df['endDT'].dt.minute
-            result['events'] = events_df.to_dict('record')
-            print(events_df.columns)
-            print(events_df.shape)
-            print(events_df.to_csv())
+            if not events_df.empty:
+                events_df = events_df.sort_values('startDT', ascending=True)
+                events_df['startDT'] = pd.to_datetime(events_df['startDT'])
+                events_df['endDT'] = pd.to_datetime(events_df['endDT'])
+                events_df['start'] = events_df['startDT'].dt
+                events_df['start_h'] = events_df['startDT'].dt.hour + 3
+                events_df['start_m'] = events_df['startDT'].dt.minute
+                events_df['end_h'] = events_df['endDT'].dt.hour + 3
+                events_df['end_m'] = events_df['endDT'].dt.minute
+                result['events'] = events_df.to_dict('record')
+                print(events_df.columns)
+                print(events_df.shape)
+                print(events_df.to_csv())
     except OperationalError as e:
         print(e)
         return render(request, "fail.html")
